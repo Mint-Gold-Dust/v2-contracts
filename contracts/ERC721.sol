2ed5b919851b9bdd7d6ac0408086d9a0062f8e44
Luiz <luiz.oamorim@gmail.com>
2023-01-24 11:03:01 -0300

    chore: Project setup
    Creation of ERC721 and MGD smart contracts. Tests are not working yet

---
+++ contracts/ERC721.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+  uint public tokenCount;
+  constructor() ERC721("Mint Gold Dust", "MGD"){}
+
+  function mint(string memory _tokenURI) external returns(uint) {
+    tokenCount++;
+    _safeMint(msg.sender, tokenCount);
+    _setTokenURI(tokenCount, _tokenURI);
+    return(tokenCount);
+  }
+}
