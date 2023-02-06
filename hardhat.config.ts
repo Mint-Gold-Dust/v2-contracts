1bca72db2d224d952867f598c327c85967803ac7
Luiz <luiz.oamorim@gmail.com>
2023-02-06 11:06:09 -0300

    test(mint): add tests to mint functionality

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,36 @@
 require("dotenv").config();
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    // goerli: {
+    //   url: "https://eth-goerli.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+    //   accounts: [privateKey1, privateKey2, ...]
+    // }
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
 
 export default config;
