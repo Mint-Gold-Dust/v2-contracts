c0ed0ebc248b296538f5b042bcf5969e901e0cea
franzquarshie@gmail.com <franzquarshie@gmail.com>
2023-02-16 17:44:10 +0000

    refactor: conflicts fix

--- test/GDNFT.ts
+++ test/GDNFT.ts
@@ -1,59 +1,59 @@
-import { expect, use } from "chai";
-import { ethers } from "hardhat";
+// import { expect, use } from "chai";
+// import { ethers } from "hardhat";
 
-const toWei = (num: any) => ethers.utils.parseEther(num.toString());
-const fromWei = (num: any) => ethers.utils.formatEther(num);
+// const toWei = (num: any) => ethers.utils.parseEther(num.toString());
+// const fromWei = (num: any) => ethers.utils.formatEther(num);
 
-describe("NFTMarketplace", function () {
-  let GDNFT: any;
-  let gdnft: any;
+// describe("NFTMarketplace", function () {
+//   let GDNFT: any;
+//   let gdnft: any;
 
-  let deployer: any;
-  let addr1: any;
-  let addr2: any;
-  let addrs: any;
+//   let deployer: any;
+//   let addr1: any;
+//   let addr2: any;
+//   let addrs: any;
 
-  let URI = "sample URI";
+//   let URI = "sample URI";
 
-  beforeEach(async function () {
-    // Get the ContractFactories and Signers here.
-    GDNFT = await ethers.getContractFactory("GDNFT");
+//   beforeEach(async function () {
+//     // Get the ContractFactories and Signers here.
+//     GDNFT = await ethers.getContractFactory("GDNFT");
 
-    [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
+//     [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // To deploy our contracts
-    gdnft = await GDNFT.deploy();
-  });
+//     // To deploy our contracts
+//     gdnft = await GDNFT.deploy();
+//   });
 
-  describe("Deployment", function () {
-    it("Should track name and symbol of the gdnft collection", async function () {
-      // This test expects the owner variable stored in the contract to be equal
-      // to our Signer's owner.
-      const gdnftName = "Gold Dust NFT";
-      const gdnftSymbol = "GDNFT";
-      expect(await gdnft.name()).to.equal(gdnftName);
-      expect(await gdnft.symbol()).to.equal(gdnftSymbol);
-    });
-  });
+//   describe("Deployment", function () {
+//     it("Should track name and symbol of the gdnft collection", async function () {
+//       // This test expects the owner variable stored in the contract to be equal
+//       // to our Signer's owner.
+//       const gdnftName = "Gold Dust NFT";
+//       const gdnftSymbol = "GDNFT";
+//       expect(await gdnft.name()).to.equal(gdnftName);
+//       expect(await gdnft.symbol()).to.equal(gdnftSymbol);
+//     });
+//   });
 
-  describe("Minting NFTs", function () {
-    it("Should track each minted NFT", async function () {
-      // addr1 mints an gdnft
-      await gdnft.connect(addr1).mint(URI);
-      expect(await gdnft.getTokenCount()).to.equal(1);
-      expect(await gdnft.balanceOf(addr1.address)).to.equal(1);
-      expect(await gdnft.tokenURI(1)).to.equal(URI);
-      // addr2 mints an gdnft
-      await gdnft.connect(addr2).mint(URI);
-      expect(await gdnft.getTokenCount()).to.equal(2);
-      expect(await gdnft.balanceOf(addr2.address)).to.equal(1);
-      expect(await gdnft.tokenURI(2)).to.equal(URI);
+//   describe("Minting NFTs", function () {
+//     it("Should track each minted NFT", async function () {
+//       // addr1 mints an gdnft
+//       await gdnft.connect(addr1).mint(URI);
+//       expect(await gdnft.getTokenCount()).to.equal(1);
+//       expect(await gdnft.balanceOf(addr1.address)).to.equal(1);
+//       expect(await gdnft.tokenURI(1)).to.equal(URI);
+//       // addr2 mints an gdnft
+//       await gdnft.connect(addr2).mint(URI);
+//       expect(await gdnft.getTokenCount()).to.equal(2);
+//       expect(await gdnft.balanceOf(addr2.address)).to.equal(1);
+//       expect(await gdnft.tokenURI(2)).to.equal(URI);
 
-      // addr1 mints an gdnft
-      await gdnft.connect(addr1).mint(URI);
-      expect(await gdnft.getTokenCount()).to.equal(3);
-      expect(await gdnft.balanceOf(addr1.address)).to.equal(2);
-      expect(await gdnft.tokenURI(3)).to.equal(URI);
-    });
-  });
-});
+//       // addr1 mints an gdnft
+//       await gdnft.connect(addr1).mint(URI);
+//       expect(await gdnft.getTokenCount()).to.equal(3);
+//       expect(await gdnft.balanceOf(addr1.address)).to.equal(2);
+//       expect(await gdnft.tokenURI(3)).to.equal(URI);
+//     });
+//   });
+// });
--- test/MGD.ts
+++ test/MGD.ts
@@ -1,145 +1,145 @@
-import { expect, use } from "chai";
-import { ethers } from "hardhat";
+// import { expect, use } from "chai";
+// import { ethers } from "hardhat";
 
-const toWei = (num: any) => ethers.utils.parseEther(num.toString());
-const fromWei = (num: any) => ethers.utils.formatEther(num);
+// const toWei = (num: any) => ethers.utils.parseEther(num.toString());
+// const fromWei = (num: any) => ethers.utils.formatEther(num);
 
-describe("MGD Smart Contract", function () {
-  let MGD: any;
-  let mgd: any;
+// describe("MGD Smart Contract", function () {
+//   let MGD: any;
+//   let mgd: any;
 
-  let GDNFT: any;
-  let gdnft: any;
+//   let GDNFT: any;
+//   let gdnft: any;
 
-  let deployer: any;
-  let addr1: any;
-  let addr2: any;
-  let addrs: any;
+//   let deployer: any;
+//   let addr1: any;
+//   let addr2: any;
+//   let addrs: any;
 
-  let _feePercent = 10;
-  let URI = "sample URI";
+//   let _feePercent = 10;
+//   let URI = "sample URI";
 
-  beforeEach(async function () {
-    // Get the ContractFactories and Signers here.
-    MGD = await ethers.getContractFactory("MGD");
-    GDNFT = await ethers.getContractFactory("GDNFT");
-    [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
+//   beforeEach(async function () {
+//     // Get the ContractFactories and Signers here.
+//     MGD = await ethers.getContractFactory("MGD");
+//     GDNFT = await ethers.getContractFactory("GDNFT");
+//     [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // To deploy our contracts
-    mgd = await MGD.deploy(_feePercent);
-    gdnft = await GDNFT.deploy();
-  });
+//     // To deploy our contracts
+//     mgd = await MGD.deploy(_feePercent);
+//     gdnft = await GDNFT.deploy();
+//   });
 
-  describe("Deployment", function () {
-    it("Should contract owner be equal to deployer address", async function () {
-      expect(await mgd.owner()).to.equal(deployer.address);
-    });
+//   describe("Deployment", function () {
+//     it("Should contract owner be equal to deployer address", async function () {
+//       expect(await mgd.owner()).to.equal(deployer.address);
+//     });
 
-    it("Should match the feePercent value with the value passed to the constructor", async function () {
-      expect(await mgd._getFeePercent()).to.equal(_feePercent);
-    });
-  });
+//     it("Should match the feePercent value with the value passed to the constructor", async function () {
+//       expect(await mgd._getFeePercent()).to.equal(_feePercent);
+//     });
+//   });
 
-  describe("Listing a NFT", function () {
-    let price = 1;
+//   describe("Listing a NFT", function () {
+//     let price = 1;
 
-    beforeEach(async () => {
-      await gdnft.connect(addr1).mint(URI);
+//     beforeEach(async () => {
+//       await gdnft.connect(addr1).mint(URI);
 
-      await gdnft.connect(addr1).setApprovalForAll(mgd.address, true);
-    });
-    it("Should track newly listed item, transfer NFT from seller to MGD marketplace and emmit the Listed event", async function () {
-      // addr1 mints an mgd
-      expect(mgd.connect(addr1)._listItem(gdnft.address, 1, toWei(price)))
-        .to.emit(mgd, "Listed")
-        .withArgs(1, gdnft.address, 1, toWei(price), addr1.address);
+//       await gdnft.connect(addr1).setApprovalForAll(mgd.address, true);
+//     });
+//     it("Should track newly listed item, transfer NFT from seller to MGD marketplace and emmit the Listed event", async function () {
+//       // addr1 mints an mgd
+//       expect(mgd.connect(addr1)._listItem(gdnft.address, 1, toWei(price)))
+//         .to.emit(mgd, "Listed")
+//         .withArgs(1, gdnft.address, 1, toWei(price), addr1.address);
 
-      // owner should be the marketplace
-      expect(await gdnft.ownerOf(1)).to.equal(mgd.address);
+//       // owner should be the marketplace
+//       expect(await gdnft.ownerOf(1)).to.equal(mgd.address);
 
-      // Item count should now equal 1
-      expect(await mgd._getTokenCount()).to.equal(1);
+//       // Item count should now equal 1
+//       expect(await mgd._getTokenCount()).to.equal(1);
 
-      // Get item from items mapping then check fields to ensure they are correct
-      const item = await mgd.items(1);
-      expect(item.itemId).to.equal(1);
-      expect(item.nft).to.equal(gdnft.address);
-      expect(item.tokenId).to.equal(1);
-      expect(item.price).to.equal(toWei(price));
-      expect(item.sold).to.equal(false);
-    });
-  });
+//       // Get item from items mapping then check fields to ensure they are correct
+//       const item = await mgd.items(1);
+//       expect(item.itemId).to.equal(1);
+//       expect(item.nft).to.equal(gdnft.address);
+//       expect(item.tokenId).to.equal(1);
+//       expect(item.price).to.equal(toWei(price));
+//       expect(item.sold).to.equal(false);
+//     });
+//   });
 
-  describe("Purchasing marketplace items", function () {
-    let price = 20;
-    let fee: any;
-    let totalPriceInWei: any;
-    beforeEach(async function () {
-      fee = (_feePercent / 100) * price;
-      // addr1 mints an nft
-      await gdnft.connect(addr1).mint(URI);
-      // addr1 approves marketplace to spend tokens
-      await gdnft.connect(addr1).setApprovalForAll(mgd.address, true);
-      // addr1 makes their gdnft a mgd item.
-      await mgd.connect(addr1)._listItem(gdnft.address, 1, toWei(price));
-    });
-    it("Should update item as sold, pay seller, transfer gdnft to buyer, charge fees and emit a Bought event", async function () {
-      const sellerInitalEthBal = await addr1.getBalance();
-      const feeAccountInitialEthBal = await deployer.getBalance();
-      // fetch items total price (market fees + item price)
-      totalPriceInWei = await mgd.getTotalPrice(1);
-      console.log("TOTALPRICE: ", +fromWei(totalPriceInWei) - fee);
-      // addr 2 purchases item.
-      await expect(
-        mgd.connect(addr2).purchaseItem(1, { value: totalPriceInWei })
-      )
-        .to.emit(mgd, "Bought")
-        .withArgs(
-          1,
-          gdnft.address,
-          1,
-          toWei(price),
-          addr1.address,
-          addr2.address
-        );
-      const sellerFinalEthBal = await addr1.getBalance();
-      const feeAccountFinalEthBal = await deployer.getBalance();
-      // Item should be marked as sold
-      expect((await mgd.items(1)).sold).to.equal(true);
-      // Seller should receive payment for the price of the gdnft sold.
-      expect(+fromWei(sellerFinalEthBal)).to.equal(
-        +price + +fromWei(sellerInitalEthBal)
-      );
-      // feeAccount should receive fee
-      expect(+fromWei(await deployer.getBalance())).to.equal(
-        fee + +fromWei(feeAccountInitialEthBal)
-      );
-      // The buyer should now own the gdnft
-      expect(await gdnft.ownerOf(1)).to.equal(addr2.address);
-    });
-    it("Should fail for invalid item ids, sold items and when not enough ether is paid", async function () {
-      // fails for invalid item ids
-      await expect(
-        mgd.connect(addr2).purchaseItem(2, { value: totalPriceInWei })
-      ).to.be.revertedWith("item doesn't exist");
-      await expect(
-        mgd.connect(addr2).purchaseItem(0, { value: totalPriceInWei })
-      ).to.be.revertedWith("item doesn't exist");
-      // Fails when not enough ether is paid with the transaction.
-      // In this instance, fails when buyer only sends enough ether to cover the price of the gdnft
-      // not the additional market fee.
-      await expect(
-        mgd.connect(addr2).purchaseItem(1, { value: toWei(price) })
-      ).to.be.revertedWith(
-        "not enough ether to cover item price and market fee"
-      );
-      // addr2 purchases item 1
-      await mgd.connect(addr2).purchaseItem(1, { value: totalPriceInWei });
-      // addr3 tries purchasing item 1 after its been sold
-      const addr3 = addrs[0];
-      await expect(
-        mgd.connect(addr3).purchaseItem(1, { value: totalPriceInWei })
-      ).to.be.revertedWith("item already sold");
-    });
-  });
-});
+//   describe("Purchasing marketplace items", function () {
+//     let price = 20;
+//     let fee: any;
+//     let totalPriceInWei: any;
+//     beforeEach(async function () {
+//       fee = (_feePercent / 100) * price;
+//       // addr1 mints an nft
+//       await gdnft.connect(addr1).mint(URI);
+//       // addr1 approves marketplace to spend tokens
+//       await gdnft.connect(addr1).setApprovalForAll(mgd.address, true);
+//       // addr1 makes their gdnft a mgd item.
+//       await mgd.connect(addr1)._listItem(gdnft.address, 1, toWei(price));
+//     });
+//     it("Should update item as sold, pay seller, transfer gdnft to buyer, charge fees and emit a Bought event", async function () {
+//       const sellerInitalEthBal = await addr1.getBalance();
+//       const feeAccountInitialEthBal = await deployer.getBalance();
+//       // fetch items total price (market fees + item price)
+//       totalPriceInWei = await mgd.getTotalPrice(1);
+//       console.log("TOTALPRICE: ", +fromWei(totalPriceInWei) - fee);
+//       // addr 2 purchases item.
+//       await expect(
+//         mgd.connect(addr2).purchaseItem(1, { value: totalPriceInWei })
+//       )
+//         .to.emit(mgd, "Bought")
+//         .withArgs(
+//           1,
+//           gdnft.address,
+//           1,
+//           toWei(price),
+//           addr1.address,
+//           addr2.address
+//         );
+//       const sellerFinalEthBal = await addr1.getBalance();
+//       const feeAccountFinalEthBal = await deployer.getBalance();
+//       // Item should be marked as sold
+//       expect((await mgd.items(1)).sold).to.equal(true);
+//       // Seller should receive payment for the price of the gdnft sold.
+//       expect(+fromWei(sellerFinalEthBal)).to.equal(
+//         +price + +fromWei(sellerInitalEthBal)
+//       );
+//       // feeAccount should receive fee
+//       expect(+fromWei(await deployer.getBalance())).to.equal(
+//         fee + +fromWei(feeAccountInitialEthBal)
+//       );
+//       // The buyer should now own the gdnft
+//       expect(await gdnft.ownerOf(1)).to.equal(addr2.address);
+//     });
+//     it("Should fail for invalid item ids, sold items and when not enough ether is paid", async function () {
+//       // fails for invalid item ids
+//       await expect(
+//         mgd.connect(addr2).purchaseItem(2, { value: totalPriceInWei })
+//       ).to.be.revertedWith("item doesn't exist");
+//       await expect(
+//         mgd.connect(addr2).purchaseItem(0, { value: totalPriceInWei })
+//       ).to.be.revertedWith("item doesn't exist");
+//       // Fails when not enough ether is paid with the transaction.
+//       // In this instance, fails when buyer only sends enough ether to cover the price of the gdnft
+//       // not the additional market fee.
+//       await expect(
+//         mgd.connect(addr2).purchaseItem(1, { value: toWei(price) })
+//       ).to.be.revertedWith(
+//         "not enough ether to cover item price and market fee"
+//       );
+//       // addr2 purchases item 1
+//       await mgd.connect(addr2).purchaseItem(1, { value: totalPriceInWei });
+//       // addr3 tries purchasing item 1 after its been sold
+//       const addr3 = addrs[0];
+//       await expect(
+//         mgd.connect(addr3).purchaseItem(1, { value: totalPriceInWei })
+//       ).to.be.revertedWith("item already sold");
+//     });
+//   });
+// });
